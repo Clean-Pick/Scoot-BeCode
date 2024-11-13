533ccf7418aebb1de4343a9423007681cdfe0790
Clean-Pick <benjamin.will98@gmail.com>
2024-11-13 09:52:25 +0100

    Tablet started

---
+++ js/includes.js
@@ -0,0 +1,14 @@
+async function loadComponent(id, file) {
+    try {
+        const response = await fetch(file);
+        if (!response.ok) throw new Error(`Erreur de chargement du fichier ${file}`);
+        const content = await response.text();
+        document.getElementById(id).innerHTML = content;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    loadComponent('header', 'header/header.html');
+});

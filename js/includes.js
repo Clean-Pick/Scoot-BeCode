4599d459e07dcaf8d9f1ba6e6ea8a5eeddac9846
Nora-H1 <nora.hashani01@gmail.com>
2024-11-14 16:53:41 +0100

    add header-in progress- & footer

--- js/includes.js
+++ js/includes.js
@@ -4,6 +4,14 @@ async function loadComponent(id, file) {
         if (!response.ok) throw new Error(`Erreur de chargement du fichier ${file}`);
         const content = await response.text();
         document.getElementById(id).innerHTML = content;
+
+        // Charge burger.js après avoir inséré le header
+        if (id === 'header') {
+            const script = document.createElement("script");
+            script.src = "./header/burger.js"; // Chemin mis à jour pour burger.js
+            script.defer = true;
+            document.body.appendChild(script);
+        }
     } catch (error) {
         console.error(error);
     }
